d6079a548ff0b180a5aee5604939e958059e21ee
Sakhura <sabina.romero@gmail.com>
2024-11-25 19:26:06 -0300

    update PV LU

--- tiendamascota/src/views/LoginUsuario.vue
+++ tiendamascota/src/views/LoginUsuario.vue
@@ -1,99 +1,72 @@
 <template>
-    <div class="login-container">
-      <h2>Iniciar sesión</h2>
-      <form @submit.prevent="handleLogin">
-        <div>
-          <label for="username">Usuario</label>
-          <input 
-            type="text" 
-            id="username" 
-            v-model="username" 
-            placeholder="Escribe tu usuario" 
-            required />
+  <div class="container mt-5">
+  <div class="card mx-auto" style="max-width: 400px;">
+    <div class="card-body">
+      <h5 class="card-title text-center">Iniciar Sesión</h5>
+      <form>
+        <div class="mb-3">
+          <label for="email" class="form-label">Email:</label>
+          <input type="email" id="email" class="form-control" v-model="user.email" placeholder="email@email.com" />
         </div>
-        <div>
-          <label for="password">Contraseña</label>
-          <input 
-            type="password" 
-            id="password" 
-            v-model="password" 
-            placeholder="Escribe tu contraseña" 
-            required />
+        <div class="mb-3">
+          <label for="password" class="form-label">Contraseña:</label>
+          <input type="password" id="password" class="form-control" v-model="user.password" />
         </div>
-        <button type="submit">Iniciar sesión</button>
+       
+        <button type="button" class="btn btn-primary w-100" @click="login()">Iniciar Sesión</button>
       </form>
-  
-      <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
+      <hr>
+    <router-link to='/RegistroUsuario' class="btn btn-primary w-100">Registrarse</router-link>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  
-  const username = ref('');
-  const password = ref('');
-  const errorMessage = ref('');
-  
-  const handleLogin = () => {
-    // Simulamos una validación simple
-    if (username.value === 'admin' && password.value === '1234') {
-      alert('Inicio de sesión exitoso');
-      errorMessage.value = ''; // Limpiar mensaje de error
-      // Aquí podrías redirigir o manejar el estado de autenticación
-    } else {
-      errorMessage.value = 'Usuario o contraseña incorrectos';
-    }
-  };
-  </script>
-  
-  <style scoped>
-  .login-container {
-    max-width: 400px;
-    margin: 0 auto;
-    padding: 20px;
-    border: 1px solid #ccc;
-    border-radius: 8px;
-    background-color: #f9f9f9;
-  }
-  
-  form {
-    display: flex;
-    flex-direction: column;
-  }
-  
-  form div {
-    margin-bottom: 12px;
-  }
-  
-  label {
-    font-weight: bold;
-    margin-bottom: 6px;
-  }
-  
-  input {
-    padding: 8px;
-    font-size: 14px;
-    width: 100%;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-  }
-  
-  button {
-    padding: 10px;
-    background-color: #4CAF50;
-    color: white;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-  }
-  
-  button:hover {
-    background-color: #45a049;
-  }
-  
-  .error-message {
-    color: red;
-    margin-top: 12px;
-  }
-  </style>
-  
+   
+  </div>
+</div>
+
+ 
+
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: "loginUser",
+  data() {
+      return {
+          user: {
+              email: '',
+              password: '',
+          }
+      }
+  },
+  methods: {
+      login() {
+          if (!this.user.email || !this.user.password) {
+              alert('Debes llenar todos los campos')
+          }
+          else {
+              axios.get(`https://raw.githubusercontent.com/shaka0241/tiendaMascotaApi/main/login.json`)
+                  .then((resp) => {
+                      if (this.user.email === resp.data.loginRequest.email && this.user.password === resp.data.loginRequest.password) {
+                          alert('logueado'),
+                          this.$router.push({ name: 'HomePage' })
+                      } else {
+                          alert('no logueado')
+                      }
+                  })
+                  .catch((error) => {
+                      console.log("error en la solicitud", error)
+                  })
+          }
+      },
+
+  },
+}
+</script>
+
+<style>
+.card {
+border: 1px solid #ddd;
+border-radius: 10px;
+box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+}
+</style>
---
+++ tiendamascota/src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+      <div class="center">
+        <img
+          src="https://webhostingmedia.net/wp-content/uploads/2018/01/http-error-404-not-found.png"
+          alt=""
+        />
+      </div>
+    </div>
+  </template>
+   
+  <style>
+  .center {
+    margin-top: 50px;
+  }
+  </style>
+   
+   
